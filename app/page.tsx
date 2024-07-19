e5f01aae4f5893143d8415a776967d32a484c33e
peteruithoven <peter@metabolic.nl>
2024-07-19 23:51:32 +0200

    Add Suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import ClientPage from "./page.client";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24  bg-white pb-10">
-      <ClientPage />
+      <Suspense>
+        <ClientPage />
+      </Suspense>
     </main>
   );
 }
