15656c5ac48d966e8fdcb1c3e1c6bbdfd5fb824b
peteruithoven <peter@metabolic.nl>
2024-08-28 23:22:12 +0200

    Remove console.logs

--- app/utils/useWakeLock.ts
+++ app/utils/useWakeLock.ts
@@ -3,14 +3,11 @@ import { useCallback, useEffect, useState } from "react";
 export default function useWakeLock() {
   const [wakeLock, setWakeLock] = useState<WakeLockSentinel | null>(null);
   const requestWakeLock = useCallback(async () => {
-    console.log("requestWakeLock");
     try {
       const wakeLock = await navigator.wakeLock.request("screen");
       wakeLock.addEventListener("release", (event) => {
-        console.log("Wake Lock was released:", event);
         setWakeLock(null);
       });
-      console.log("  wakeLock: ", wakeLock);
       setWakeLock(wakeLock);
     } catch (err) {
       console.log("Wake Lock couldn't be activated", err);
@@ -18,16 +15,12 @@ export default function useWakeLock() {
   }, []);
 
   const visibilityChangeHandler = useCallback(async () => {
-    console.log("visibilityChangeHandler: ");
-    console.log("  document.visibilityState: ", document.visibilityState);
-    console.log("  document.hidden: ", document.hidden);
     if (document.visibilityState === "visible") {
       requestWakeLock();
     }
   }, [requestWakeLock]);
 
   useEffect(() => {
-    console.log("useEffect");
     requestWakeLock();
     document.addEventListener("visibilitychange", visibilityChangeHandler);
 
