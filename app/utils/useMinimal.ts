63eb8898d2a5dd84aa50d4436e2a4d0e3e07a31a
peteruithoven <peter@metabolic.nl>
2024-09-19 19:21:46 +0200

    Cleanup

--- app/utils/useMinimal.ts
+++ app/utils/useMinimal.ts
@@ -5,6 +5,5 @@ const KEY = "minimal";
 export default function useMinimal() {
   const searchParams = useSearchParams();
   const value = searchParams.get(KEY);
-  console.log("value: ", value);
   return value === "" || value === "true";
 }
