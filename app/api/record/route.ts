3a2f257e552d4a71a90f3342d8afc863c6c26c38
peteruithoven <peter@metabolic.nl>
2024-09-24 01:38:33 +0200

    Keep record

---
+++ app/api/record/route.ts
@@ -0,0 +1,17 @@
+import { sql } from "@vercel/postgres";
+import scrapeData from "@/app/utils/scrapeData";
+export const dynamic = "force-dynamic";
+
+export const runtime = "nodejs";
+
+const HISTORY_PROJECT = process.env.HISTORY_PROJECT;
+console.log("HISTORY_PROJECT: ", HISTORY_PROJECT);
+
+export async function GET() {
+  if (!HISTORY_PROJECT) return;
+  const data = await scrapeData({ project: HISTORY_PROJECT });
+  const { funded } = data;
+  console.log("funded: ", funded);
+  await sql`INSERT INTO history (timestamp, funded) VALUES (NOW(), ${funded});`;
+  return new Response(`Recorded ${funded} at ${new Date()}`);
+}
