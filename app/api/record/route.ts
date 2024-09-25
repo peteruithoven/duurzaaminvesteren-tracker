d418798ba92209caa16e78db6aa27d0d92d66b0a
peteruithoven <peter@metabolic.nl>
2024-09-25 16:21:05 +0200

    Fix record API

--- app/api/record/route.ts
+++ app/api/record/route.ts
@@ -3,21 +3,21 @@ import scrapeData from "@/app/utils/scrapeData";
 export const dynamic = "force-dynamic";
 
 export const runtime = "nodejs";
+export const revalidate = 0;
 
-const HISTORY_PROJECT = process.env.HISTORY_PROJECT;
-console.log("HISTORY_PROJECT: ", HISTORY_PROJECT);
+const project = process.env.HISTORY_PROJECT;
+console.log("project: ", project);
 
 export async function GET() {
-  if (!HISTORY_PROJECT) return;
-  // Add caching layer...
-  const data = await scrapeData({ project: HISTORY_PROJECT });
-
+  if (!project) return;
+  // TODO Add caching layer...
+  const data = await scrapeData({ project });
   const { funded } = data;
-  console.log("funded: ", funded);
-  await sql`INSERT INTO history (timestamp, funded) VALUES (NOW(), ${funded});`;
-  console.log(`Recorded ${funded} at ${new Date()}`);
-  const response = await sql`SELECT COUNT(*) AS row_count FROM history;`;
-  console.log("count response: ", response);
-  return new Response(`Recorded ${funded} at ${new Date()}`);
+
+  const result =
+    await sql`INSERT INTO history (timestamp, funded, project) VALUES (NOW(), ${funded}, ${project}) RETURNING *;`;
+  console.log("Inserted row:", result.rows[0]);
+
+  return Response.json({ row: result.rows[0] });
 }
 //
